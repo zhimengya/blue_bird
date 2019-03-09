9b8f7b9619250a55c908d04e2a3a301520160da3
xbw <15938887104@163.com>
2019-03-09 11:05:53 +0800

    commit

--- app/src/main/java/com/kgc/tiku/bluebird/entity/GradeItem.kt
+++
@@ -1,8 +0,0 @@
-package com.kgc.tiku.bluebird.entity
-
-class GradeItem constructor(var itemName: String, var itemUrl: String) {
-
-    override fun toString(): String {
-        return itemName
-    }
-}
--- app/src/main/java/com/kgc/tiku/bluebird/entity/Ranking.kt
+++
@@ -1,9 +0,0 @@
-package com.kgc.tiku.bluebird.entity
-
-class Ranking {
-    var userName: String? = null
-    var percentage: String? = null
-    var index: Int = 0
-    var answerNo:Int=0
-    var actualQuestionNoNow:Int=0
-}
